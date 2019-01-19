231df13f458fc5b47bfd5de682c3d013aae6f3ac
Abbet <abbetwang@gmail.com>
2019-01-19 10:28:03 +0800

    commet the bean shows

--- src/main/java/check/Application.java
+++ src/main/java/check/Application.java
@@ -21,11 +21,11 @@ public class Application {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+            // String[] beanNames = ctx.getBeanDefinitionNames();
+            // Arrays.sort(beanNames);
+            // for (String beanName : beanNames) {
+            //     System.out.println(beanName);
+            // }
 
         };
     }
