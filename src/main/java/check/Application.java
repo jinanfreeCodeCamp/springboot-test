13b5efb6a61bfbeb58168b514f21ebe0f43e0959
Abbet <abbetwang@gmail.com>
2019-01-23 18:06:22 +0800

    alter

--- src/main/java/check/Application.java
+++ src/main/java/check/Application.java
@@ -3,11 +3,14 @@ package check;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
