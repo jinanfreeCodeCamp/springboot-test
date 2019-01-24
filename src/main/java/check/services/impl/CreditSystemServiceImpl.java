fca5eab98f62edbd6ed6d86d415814c99bdc7e8b
Abbet <abbetwang@gmail.com>
2019-01-24 13:11:51 +0800

    add CreditSystemService

---
+++ src/main/java/check/services/impl/CreditSystemServiceImpl.java
@@ -0,0 +1,20 @@
+package check.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import check.services.CreditSystemService;
+
+@Service
+public class CreditSystemServiceImpl implements CreditSystemService {
+
+    @Override
+    public int getUserCredit(int count) {
+        return 0;
+    }
+
+    @Override
+    public int getCredit(int id) {
+        return 0;
+    }
+
+}
